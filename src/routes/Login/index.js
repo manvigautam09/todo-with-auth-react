f5a554ad42f2beec10d074b107e41ffd67768032
ManviSharma09 <manvis@geekyants.com>
2020-05-01 14:36:27 +0530

    scss setting

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./style.scss";
 const Login = () => {
     return (
-        <div>This is Login</div>
+        <div className="app-container">This is Login</div>
     )
 }
 export default Login;
